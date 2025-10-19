2add59532f43dee3a596188d47b27de317b7d135
anth0nyun <anthonyun72@gmail.com>
2025-10-18 20:10:26 -0400

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import WorkoutsList from './pages/WorkoutsList';
+import WorkoutDetail from './pages/WorkoutDetail';
+import WorkoutForm from './pages/WorkoutForm';
 
 export default function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<WorkoutsList />} />
+        <Route path="/workouts/new" element={<WorkoutForm />} />
+        <Route path="/workouts/:id" element={<WorkoutDetail />} />
+        <Route path="/workouts/:id/edit" element={<WorkoutForm />} />
       </Routes>
     </Router>
   );
